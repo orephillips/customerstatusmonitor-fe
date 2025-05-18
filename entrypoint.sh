54a23fa00e355ce6ee31866a5f12b4af39f551fc
Ore Phillips <ore.phillips@bitwave.io>
2025-05-17 23:55:43 -0400

    Fix entrypoint.sh to correctly replace port 8080 in nginx config

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ echo "PORT=${PORT}"
 # Replace the port in nginx config
 if [ -n "$PORT" ]; then
   echo "Configuring nginx to listen on port $PORT..."
-  sed -i.bak "s|listen 80;|listen $PORT;|g" /etc/nginx/conf.d/default.conf
+  sed -i.bak "s|listen 8080;|listen $PORT;|g" /etc/nginx/conf.d/default.conf
   cat /etc/nginx/conf.d/default.conf
 fi
 
